ad38d2c0c83c0a31043baed8529ac82f47ac53ec
Tran Tâm-Tanguy <thanh-tam-tangu.tran@epita.fr>
2021-01-31 00:38:52 +0100

    feat: Update usage

--- client/main.go
+++ client/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "discord-sound/youtubedl"
+import (
+	"discord-sound/youtubedl"
+	"flag"
+)
 
 func main() {
-	youtubedl.Start()
+
+	flag.Parse()
+
+	args := flag.Args()
+
+	if len(args) == 0 {
+		flag.Usage()
+		return
+	}
+
+	switch args[0] {
+	case "youtubedl":
+		youtubedl.Start()
+	default:
+		flag.Usage()
+		return
+	}
+
 }
